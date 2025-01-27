c01333bf05e81ec0a3955aba1c5e0afdff69a9e0
Sofus Addington <sofus@addington.dk>
2025-01-27 14:33:42 +0100

    Simplify flake. Still does not work

--- flake.nix
+++ flake.nix
@@ -1,22 +1,10 @@
 {
-  description = "Build a cargo project";
+  description = "Build a cargo project without extra checks";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      # inputs.rust-analyzer-src.follows = "";
-    };
-
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-
   };
 
   outputs = { self, nixpkgs, crane, flake-utils, ... }:
@@ -24,101 +12,49 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        inherit (pkgs) lib;
-
         craneLib = crane.mkLib pkgs;
-        src = craneLib.cleanCargoSource (craneLib.path ./.);
 
-        # Common arguments can be set here to avoid repeating them later
         commonArgs = {
-          inherit src;
+          src = craneLib.cleanCargoSource ./.;
           strictDeps = true;
 
-          buildInputs = with pkgs; [
-            gst_all_1.gstreamer
-            gst_all_1.gst-plugins-base
-            glib
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            libiconv
+          buildInputs = [
+            pkgs.gst_all_1.gstreamer
+            pkgs.gst_all_1.gst-plugins-base
+            pkgs.glib
           ];
 
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            pcre2
-            libunwind
-            elfutils
-            binaryen
+          nativeBuildInputs = [
+            pkgs.pkg-config
           ];
-
         };
 
-        # craneLibLLvmTools = craneLib.overrideToolchain
-        #   (fenix.packages.${system}.complete.withComponents [
-        #     "cargo"
-        #     "rustup"
-        #   ]);
-
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
         qobuz-player = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
         });
       in
       {
         checks = {
-          # Build the crate as part of `nix flake check` for convenience
           inherit qobuz-player;
-
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          qobuz-player-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
-
-          qobuz-player-doc = craneLib.cargoDoc (commonArgs // {
-            inherit cargoArtifacts;
-          });
-
-          # Check formatting
-          qobuz-player-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
         };
 
-        packages = {
-          default = qobuz-player;
-        }; 
-        
+        packages.default = qobuz-player;
+
         apps.default = flake-utils.lib.mkApp {
           drv = qobuz-player;
         };
 
         devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
           checks = self.checks.${system};
 
-          packages = with pkgs; [
-            rustup
-            llvmPackages.lld
-            gst_all_1.gstreamer.dev
-            gst_all_1.gst-plugins-base.dev
-            glib.dev
-            cargo-machete
-            sqlx-cli
+          packages = [
+            pkgs.cargo-machete
+            pkgs.sqlx-cli
+
           ];
 
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-          DATABASE_URL = "sqlite:///tmp/data.db";
-          
+        DATABASE_URL = "sqlite:///tmp/data.db";
         };
       });
 }
